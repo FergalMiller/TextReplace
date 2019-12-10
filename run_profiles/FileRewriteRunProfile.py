daf8d49250b065e06fc0097c984dc5835adcf67d
Fergal Miller <fergal.miller@cognitran.com>
2019-12-10 13:34:27 +0000

    Initial implementation of single file rewrite profile added Refactored unicode escape schema generator

--- run_profiles/FileRewriteRunProfile.py
+++ run_profiles/FileRewriteRunProfile.py
@@ -26,8 +26,26 @@ class SingleFileRewriteRunProfile(RunProfile):
     arguments: List[Argument] = [Argument("-path", "The file path.", True)]
 
     def run(self, schema: Dict[str, str], arguments: Dict[str, str]):
-        # TODO
-        print(self.arguments)
+        try:
+            self.validate_arguments(arguments)
+        except ArgumentError as e:
+            print("Argument error: " + e.reason)
+            return
+
+        target_file_path = arguments["-path"]
+        print('\033[92m' + "Replacing illegal characters in `" + target_file_path + "`" + '\033[0m')
+
+        with open(target_file_path, "r") as target_file:
+            lines = target_file.readlines()
+            index = 0
+            for line in lines:
+                for illegal_character in schema.keys():
+                    line = line.replace(illegal_character, schema[illegal_character])
+                lines[index] = line.__str__()
+                index += 1
+
+        with open(target_file_path, "w") as target_file:
+            target_file.writelines(lines)
 
     @staticmethod
     def command() -> str: return "-s"
